b07243aca76a023866608cac682efb369ba5ad8c
voodoo <voodoo@blackandyellowpages.com>
2014-07-23 12:27:54 -0500

    fixed reference to wrong app.js and module definition

--- README.md
+++
@@ -1,4 +0,0 @@
-cambrian-app-skel
-=================
-
-Default HTML5 Angular JS Cambrian App Skeleton for Seeding New Projects
--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
-appModule = angular.module("app", function () {
-	
+appModule = angular.module("app", [])
+.controller("appCtrl", function ($scope) {
+  // controller logic go here
 });
