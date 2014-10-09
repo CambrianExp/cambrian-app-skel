0c6e6c918b436ebb6741ac16d4072ec3d0b9c430
Jimmy <jimmy@societypro.org>
2014-10-09 11:45:41 -0600

    added version

--- js/app.js
+++ js/app.js
@@ -1,4 +1,7 @@
-appModule = angular.module("app", [])
-.controller("appCtrl", function ($scope) {
+appModule = angular.module("app", ['ngMaterial','ngAria'])
+.controller("appCtrl", function ($scope,$materialSidenav) {
   // controller logic go here
+  	$scope.toggleMenu = function () {
+	  $materialSidenav('left').toggle();
+	};
 });
